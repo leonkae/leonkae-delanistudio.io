f30c8b4f100c1e3b5ff0f476d37c8b769e1147fb
leonkae <leon29kae@gmail.com>
2022-03-11 20:26:20 +0300

    adding text

---
+++ main.js
@@ -0,0 +1,21 @@
+// $(document).ready (function(){
+//     $("#button-element").click(function(){
+//         $("h2").css("color", "dodgerblue")
+$(document).ready(function(){
+  $("#icon1").click(function(){
+      $(".text-1").toggle()
+  });
+});
+
+$(document).ready(function () {
+  $("#icon2").click(function () {
+    $(".text-2").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#icon3").click(function () {
+    $(".text-3").toggle();
+  });
+});
+ 
