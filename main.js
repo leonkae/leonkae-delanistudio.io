fe8055714cf557878c6e06c4ffc69bfd5a9eb00a
leonkae <leon29kae@gmail.com>
2022-03-12 00:52:05 +0300

    completed service

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-
-
-
-$(document).ready(function(){
-  $("#icon1").click(function(){
-      $(".text-1").toggle()
-  });
-});
-
 $(document).ready(function () {
-  $("#icon2").click(function () {
-    $(".text-2").toggle();
-  });
+  toggleImage();
 });
 
-$(document).ready(function () {
-  $("#icon3").click(function () {
-    $(".text-3").toggle();
+// $(document).ready(function(){
+//   $("#icon1").click(function(){
+//       $(".text-1").toggle()
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#icon2").click(function () {
+//     $(".text-2").toggle();
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#icon3").click(function () {
+//     $(".text-3").toggle();
+//   });
+// });
+
+function toggleImage() {
+  $(".image-wrapper p").hide();
+  $(".image-wrapper").click(function () {
+    $(this).find("img").toggle(300);
+    $(this).find("p").toggle(300);
   });
-});
- 
-
-
+}
